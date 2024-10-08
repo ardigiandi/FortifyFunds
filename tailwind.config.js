fcd629591524de7ae3c5ba09adf976d0bd256e34
ardigiandi <ardiansyahg9080@gmail.com>
2024-10-08 20:04:01 +0700

    add : landing page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'abu': '#EBEEF1',
+        'biru': '#304966'
+      }
+    },
+  },
+  plugins: [],
+}
+
